190facfcc4c35bbcd2413ecf1a2f9812047a22b7
vanshika.gupta1 <vanshika.gupta1@cleartrip.com>
2025-08-21 17:41:24 +0530

    Revert "removed key"
    This reverts commit 2affe78805232dd47b5b7215142568ad4b7a62d6.

--- controllers/smart_package_controller.py
+++ controllers/smart_package_controller.py
@@ -392,8 +392,8 @@ def call_genai_api(prompt):
     """
     try:
         # GenAI API configuration
-        api_url = ""
-        api_data = ""
+        api_url = "https://hackathon-openui-test.openai.azure.com/openai/deployments/gpt-4o/chat/completions?api-version=2025-01-01-preview"
+        api_key = ""
         
         headers = {
             'Content-Type': 'application/json',
